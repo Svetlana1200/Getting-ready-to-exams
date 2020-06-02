251ab8bb4ec74d03f92e21f2d7ec89c82d7e2cda
mariadyuzheva <mdyuzheva@gmail.com>
2020-06-02 22:39:34 +0500

    Улучшено сравнение ответов в задании "Предложения", убрано автодополнение при вводе ответа

--- EnglishTest/Models/SentenceAnswer.cs
+++ EnglishTest/Models/SentenceAnswer.cs
@@ -1,4 +1,6 @@
-﻿namespace EnglishTest.Models
+﻿using System.Collections.Generic;
+
+namespace EnglishTest.Models
 {
     public class SentenceAnswer : IAnswer
     {
@@ -11,6 +13,19 @@
         public readonly string Class;
         public int Count { get; private set; }
 
+        private readonly Dictionary<string, List<string>> verbContractions = new Dictionary<string, List<string>>()
+        {
+            { "'m", new List<string>(){" am" } },
+            { "'re", new List<string>(){" are" } },
+            { "'s", new List<string>(){" is", " has" } },
+            { "n't", new List<string>(){" not" } },
+            { "can't", new List<string>(){"cannot" } },
+            { "won't", new List<string>(){"will not" } },
+            { "'d", new List<string>(){" would", " had" } },
+            { "'ve", new List<string>(){" have" } },
+            { "'ll", new List<string>(){" will"} }
+        };
+
         public SentenceAnswer(string first, string second, string word, string rightAnswer, string userAnswer)
         {
             First = first;
@@ -18,6 +33,7 @@
             Word = word;
             RightAnswer = rightAnswer;
             UserAnswer = userAnswer;
+
             if (string.Compare(RightAnswer, UserAnswer, true) != 0)
                 Class = "wrong";
         }
@@ -25,10 +41,25 @@
         public bool IsRight()
         {
             Count = 0;
-            if (string.Compare(RightAnswer, UserAnswer, true) == 0)
+
+            if (string.Compare(RightAnswer, UserAnswer.Trim(), true) == 0)
             {
                 Count = MaxCount;
                 return true;
+            }
+            else
+            {
+                foreach (var contraction in verbContractions)
+                    if (UserAnswer.Contains(contraction.Key))
+                        foreach (var variant in contraction.Value)
+                        {
+                            var userAnswer = UserAnswer.Replace(contraction.Key, variant).Trim();
+                            if (string.Compare(RightAnswer, userAnswer, true) == 0)
+                            {
+                                Count = MaxCount;
+                                return true;
+                            }
+                        }
             }
             return false;
         }
