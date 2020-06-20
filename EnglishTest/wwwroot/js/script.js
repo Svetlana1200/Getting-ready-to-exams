8b0d5579db0a9723a295ba5ef79ed1a7d038455f
mariadyuzheva <mdyuzheva@gmail.com>
2020-06-20 12:45:54 +0500

    Код на JS перенесён в отдельный файл

---
+++ EnglishTest/wwwroot/js/script.js
@@ -0,0 +1,22 @@
+﻿$("input[name='UserCondition']").change(function () {
+    if ($(this).val() == "Timer") {
+        $("#Time").show();
+    }
+    else {
+        $("#Time").hide();
+        $("#Time").val(1);
+    }
+});
+
+$("input[name='UserLevel']").change(function () {
+    if ($(this).val() == "B1") {
+        $("#Images").parent().show();
+    }
+    else {
+        $("#Images").parent().hide();
+        if ($("#Images").is(":checked")) {
+            $("#All").prop("checked", true);
+        }
+    }
+});
+
